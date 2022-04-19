f6e64a8e4aba2a2625f6264ebf9403a5465ce656
ronya78@gmail.com <ronya78@gmail.com>
2022-04-19 23:56:26 +0300

    Loading mongoosejs framework and creat two new collections of database.

--- app.js
+++ app.js
@@ -1,34 +1,97 @@
 //jshint esversion:6
 
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-// Connection URI
-const uri =
-  "mongodb://localhost:27017"
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    // await client.db("fruitsDB").command({ ping: 1 });
-    // console.log("Connected successfully to server");
-    const database = client.db("fruitsDB");
-    const foods = database.collection("fruits");
-    // create an array of documents to insert
-    const docs = [
-      { name: "cake", healthy: false },
-      { name: "lettuce", healthy: true },
-      { name: "donut", healthy: false }
-    ];
-    // this option prevents additional documents from being inserted if one fails
-    const options = { ordered: true };
-    const result = await foods.insertMany(docs, options);
-    console.log(`${result.insertedCount} documents were inserted`);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+//insert some database
+//1. To create a new schema
+const fruitsSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
+
+//2. To cretae a mongoose model
+const Fruit = mongoose.model("Fruit", fruitsSchema);
+
+//3. To create a new fruit document
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solidas a fruit."
+});
+
+//4. To calls the SAVE method in Mongoose to save this
+// - fruit document into a Fruit collection inside a fruitsDB
+// fruit.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "John",
+  age:37
+});
+
+person.save();
+
+// Add new documents
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 10,
+  review: "The best fruit."
+});
+const orange = new Fruit({
+  name: "Orange",
+  score: 3,
+  review: "Too sour for me."
+});
+const banana = new Fruit({
+  name: "Banana",
+  score: 3,
+  review: "Weird texture."
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+  if(err){
+    console.log(err);
+  }else {
+    console.log("Succesfully saved all the fruits to FruitsDB.");
   }
-}
-run().catch(console.dir);
+});
+
+
+
+// const insertDucuments = function(db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection("fruits");
+//   // Insert same documents
+//   collection.insertMany([
+//     {
+//       name: "Apple",
+//       rating: 7,
+//       review: "Pretty solidas a fruit."
+//     }
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     callback(result);
+//   });
+// };
+
+// const findDocuments = function(db, callback) {
+//   // Get the document collection
+//   const collection = db.collection("fruits");
+//   // Find same findDocuments
+//   collection.find({}).toArray(function(err, fruits) {
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(fruits);
+//     callback(fruits);
+//   });
+// };
